91435595dc3b60f112690ab7850f8ddfd2427f66
Bartu10 <jbg10102002@gmail.com>
2023-02-03 09:55:37 +0100

    tercer commit

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,7 +2,12 @@
     <h1 v-if="!pokemon">Espere por favor...</h1>
 
     <div v-else>
-        <h2>Puntuacion: {{this.puntuacion}}</h2>
+        
+        <div>
+            <h2 >Vidas:</h2>
+            <img v-for="n in this.vidas" v-bind:key="n" style="width:20px" src="../assets/kisspng-love-heart-love-heart-romance-clip-art-picture-of-red-heart-5aaeb718420cb8.8640685015213995762706.png" />
+        </div>
+        <h1>Puntuacion: {{this.puntuacion}}</h1>
         <h1>¿Quién es este pokémon?</h1>
         
         <PokemonPicture 
@@ -10,14 +15,17 @@
             :show-pokemon="showPokemon" 
         />
 
-        <PokemonOptions 
+        <PokemonOptions
+            v-if="!showAnswer"
             :pokemons="pokemonArr"
             @selection-pokemon="checkAnswer"
         />
 
         <template v-if="showAnswer">
             <h2 class="fade-in">{{ message }}</h2>
-            {{ setTimeout(newGame(),5) }}
+            <button @click="newGame">
+                Next
+            </button>
         </template>
 
     </div>
@@ -27,7 +35,6 @@
 <script>
 import PokemonOptions from '@/components/PokemonOptions.vue'
 import PokemonPicture from '@/components/PokemonPicture.vue'
-
 import getPokemonOptions from '@/helpers/getPokemonOptions'
 
 
@@ -35,6 +42,8 @@ export default {
     components: { PokemonOptions, PokemonPicture },
     data() {
         return {
+            contador: 0,
+            vidas: 3,
             puntuacion: 0,
             pokemonArr: [],
             pokemon: null,
@@ -57,19 +66,33 @@ export default {
             if( selectedId === this.pokemon.id ) {
                 this.message = `Correcto, ${ this.pokemon.name }`
                 this.puntuacion += 1
+                this.contador += 1
+                if(this.contador == 10){
+                    if(this.vidas < 5){
+                    this.vidas += 1                        
+                    }
+                    this.contador = 0
+                }
             } else {
                 this.message = `Oops, era ${ this.pokemon.name }`
-                this.puntuacion = 0
+                this.vidas -= 1
+                if(this.vidas == 0){
+                    this.reset()
+                }
             }
         },
         newGame() {
-            
             this.showPokemon = false
             this.showAnswer  = false
             this.pokemonArr  = []
             this.pokemon     = null
             this.mixPokemonArray()            
 
+        }
+,
+        reset(){
+            this.vidas = 3
+            this.puntuacion = 0
         }
     },
     mounted() {
